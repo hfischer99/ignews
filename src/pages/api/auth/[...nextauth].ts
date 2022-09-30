74457eaab0450da2f8d40a101b5f1a8c6354309d
hischer99 <bernardfischer99@hotmail.com>
2022-09-29 22:14:57 -0300

    next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import GitHubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth/next";
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user",
+        },
+      },
+    }),
+  ],
+});
